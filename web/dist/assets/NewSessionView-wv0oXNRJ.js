79a6e0aa8f9ba22ff3f221b51e12d214b4d54834
鲍宇云 <yuyun.bao@bizops.com.cn>
2026-07-24 17:03:28 +0800

    chore(web): 重建 dist（全局 SSE 合并）

--- web/dist/assets/NewSessionView-wv0oXNRJ.js
+++ web/dist/assets/NewSessionView-wv0oXNRJ.js
@@ -1,4 +1,4 @@
-import{u as _,h as M,r as n,j as e}from"./react-vendor-CS42I0cf.js";import{c as B,l as T,b as V,L as F,I as H,B as c,S as z,F as q}from"./index-Dxz872cw.js";import{S as i,a as d,b as o,c as m,d as l}from"./select-B1KOh5Nq.js";import"./base-ui-xjLxUYMv.js";/**
+import{u as _,h as M,r as n,j as e}from"./react-vendor-CS42I0cf.js";import{c as B,l as T,b as V,L as F,I as H,B as c,S as z,F as q}from"./index-B0WZw_yz.js";import{S as i,a as d,b as o,c as m,d as l}from"./select-WEgZV3Zg.js";import"./base-ui-xjLxUYMv.js";/**
  * @license lucide-react v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
