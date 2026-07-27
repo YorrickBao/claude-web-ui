6d9d954fb3d7a422c623b1bba5060df00088cb83
鲍宇云 <yuyun.bao@bizops.com.cn>
2026-07-27 18:28:56 +0800

    chore(web): 重建 dist（全局化 profile 列表）

--- web/dist/assets/badge-CjyRkK3Y.js
+++ web/dist/assets/badge-CjyRkK3Y.js
@@ -1,4 +1,4 @@
-import{c as r,j as n,G as d}from"./index-DuRJYDbj.js";import{u as o,m as s}from"./base-ui-xjLxUYMv.js";/**
+import{c as r,j as n,J as d}from"./index-DJvT275z.js";import{u as o,m as s}from"./base-ui-xjLxUYMv.js";/**
  * @license lucide-react v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
